b0f23fa4f40ac206df37b48f029d0fe400441741
li-deepa <li.deepa02@gmail.com>
2022-05-14 17:43:36 -0700

    added models and image settings

--- store/views.py
+++ store/views.py
@@ -1,10 +1,10 @@
 from multiprocessing import context
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 def store(request):
-
-    context={}
+    products=Product.objects.all()
+    context={'products':products}
     return render(request,'store/store.html',context)
 
 
